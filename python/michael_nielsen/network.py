00342b91c6d31c9a6c4add150b7f34aec740c205
shawn <shawn24601@gmail.com>
2016-11-08 19:19:49 +0800

    add test

--- python/michael_nielsen/network.py
+++ python/michael_nielsen/network.py
@@ -6,6 +6,15 @@ import numpy as np
 class Network(object):
 
     def __init__(self, sizes):
+        """The list ``sizes`` contains the number of neurons in the respective
+        layers of the network. For example, if the list was [2, 3, 1] then it
+        would be a three-layer network, with the first layer containing 2
+        neurons, the second layer 3 neurons, and the third layer 1 neuron. The
+        biases and weights for the network are initialized randomly, using a
+        Gaussian distribution with mean 0, and variance 1. Note that the first
+        layer is assumed to be an input layer, and by convention we won't set
+        any biases for those neurons, since biases are only ever used in
+        computing the outputs from later layers."""
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -41,15 +50,21 @@ class Network(object):
         n = len(training_data)
         for j in xrange(epochs):
             np.random.shuffle(training_data)
-            #mini_batches = [
-            #    training_data[k:k+mini_batch_size]
-            #    for k in xrange(0, n, mini_batch_size) ]
-            #for mini_batch in mini_batches:
+            i_update = 0
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size) ]
+            for mini_batch in mini_batches:
+                print "update_mini_batch_times: {}".format(i_update)
+                i_update += 1
+                self.update_mini_batch(mini_batch, eta)
+
+            #for k in xrange(0, n, mini_batch_size):
+            #    mini_batch = training_data[k:k+mini_batch_size]
+            #    print "update_mini_batch_times: {}".format(i_update)
+            #    i_update += 1
             #    self.update_mini_batch(mini_batch, eta)
 
-            for k in xrange(0, n, mini_batch_size):
-                mini_batch = training_data[k:k+mini_batch_size]
-                self.update_mini_batch(mini_batch, eta)
             if test_data:
                 print "Epoch {0}: {1} / {2}".format(
                     j, self.evaluate(test_data), n_test)
@@ -134,5 +149,16 @@ def main():
     n = Network([2, 3, 1])
     n.printSelf()
 
+def test():
+    import mnist_loader
+    training_data, validation_data, test_data = \
+        mnist_loader.load_data_wrapper()
+
+    net = Network([784, 30, 10])
+    print np.shape(training_data)[0]
+    net.SGD(training_data, 1, 10, 3.0, test_data=test_data)
+    #net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
 if __name__ == "__main__":
-    main()
+    #main()
+    test()
